850d5fc75d35f01830089bf458ec3a9b2570fee9
farid2016 <farid.juve@gmail.com>
2021-05-03 19:06:55 +0430

    Email-Client

---
+++ app/auth/auth-http-intercptor.spec.ts
@@ -0,0 +1,7 @@
+import { AuthHttpIntercptor } from './auth-http-intercptor';
+
+describe('AuthHttpIntercptor', () => {
+  it('should create an instance', () => {
+    expect(new AuthHttpIntercptor()).toBeTruthy();
+  });
+});
